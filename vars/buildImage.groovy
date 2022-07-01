c634fc8b50ab93431321eafd9aebc7bc0407d044
odin <odin@nygard.io>
2022-07-01 14:41:40 +0200

    change buildimage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 def call () {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t alzaron/demo-app:test-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push alzaron/demo-app:test-2.0'
     }
 } 
